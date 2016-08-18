6e830f5a196db0a0786b2679beb6fdd07fa3d164
Damien Clark <damo.clarky@gmail.com>
2016-08-19 09:34:16 +1000

    Initial commit of working version

---
+++ Moodle_Course_Searcher.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        Moodle Course Searcher
+// @namespace   http://damos.world
+// @description Search for any course site on any page from textbook in Moodle header
+// @include     https://moodle.cqu.edu.au/*
+// @include     https://moodle-vet.cqu.edu.au/*
+// @include     https://moodle-archive-2014.cqu.edu.au/*
+// @version     0.1.0
+// @grant       none
+// ==/UserScript==
+
+console.log('Running Moodle Course Search Header') ;
+
+//Here is the standard Moodle 2.7 Search form HTML from existing pages in Moodle
+var courseSearchHTML = '<div class="navbutton"> <form method="get" action="https://moodle.cqu.edu.au/course/search.php" id="coursesearchnavbar"><fieldset class="coursesearchbox invisiblefieldset"><label for="navsearchbox">Search courses: </label><input type="text" value="" name="search" size="20" id="navsearchbox"><input type="submit" value="Go"></fieldset></form></div>' ;
+
+//Stick it into the Moodle Header - Voila!!
+console.log('About to insert '+courseSearchHTML) ;
+document.querySelector("#page-header").insertAdjacentHTML("beforeend",courseSearchHTML) ;
+//Hack for CQU Moodle sites that have a banner image in the header 
+document.querySelector("img#custombanner").style.zIndex = "-1" ;
